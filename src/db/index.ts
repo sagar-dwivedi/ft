0a70572313b7c8764de9c76f729757454f197a8f
sagar-dwivedi <sagar.dw20@gmail.com>
2025-08-11 23:16:20 +0530

    Fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.NETLIFY_DATABASE_URL!);
